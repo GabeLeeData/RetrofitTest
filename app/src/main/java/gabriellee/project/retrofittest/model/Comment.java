58a0b302e29dd3535ba4f8a193a7d9c2365f7d78
GabeLeeData <gabe95sk@gmail.com>
2019-04-06 21:39:57 -0400

    Initial commit

---
+++ app/src/main/java/gabriellee/project/retrofittest/model/Comment.java
@@ -0,0 +1,34 @@
+package gabriellee.project.retrofittest.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Comment {
+
+    private int postId;
+    private int id;
+    private String email;
+    private String name;
+
+    @SerializedName("body")
+    private String text;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
